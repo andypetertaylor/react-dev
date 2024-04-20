0aab3483a47fe0d425f8d337039c955b725d8d76
andypetertaylor <87092661+andypetertaylor@users.noreply.github.com>
2024-04-20 20:33:38 +0100

    Commit

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,11 +1,27 @@
 import { dbQuery } from '../data/dbQuery';
+import * as React from 'react';
+import { DataGrid } from '@mui/x-data-grid';
 
 export const Main = () => {
     const tasks = dbQuery('get', 'task');
-    console.log(tasks);
+    const columns = [
+        { field: 'id', headerName: 'ID', width: 70 },
+        { field: 'title', headerName: 'Title', width: 130 },
+        { field: 'description', headerName: 'Description', width: 130 },
+    ];
     return (
-        <>
-            Tasks: {tasks.length}
-        </>
+        <div style={{ height: 400, width: '100%' }}>
+            <DataGrid
+            rows={tasks}
+            columns={columns}
+            initialState={{
+                pagination: {
+                paginationModel: { page: 0, pageSize: 5 },
+                },
+            }}
+            pageSizeOptions={[5, 10]}
+            checkboxSelection
+            />
+        </div>
     );
 }
